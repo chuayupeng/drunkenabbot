2429226d6119b245793b509cad3f7c009e7282ae
chuayupeng <chua.yupeng@dhs.sg>
2018-10-12 23:48:09 +0800

    First commit

---
+++ app.js
@@ -0,0 +1,19 @@
+var TelegramBot = require('node-telegram-bot-api');
+var Markov = require('markov');
+var FS = require('fs');
+var token = "578851495:AAGQGxFJCLK267YDKetJmxIs_YrfWMMdVSk";
+var bot = new TelegramBot(token, { polling: true });
+
+var dresdenMarkov = Markov();
+var stream = FS.createReadStream(__dirname + '/dresden.txt');
+dresdenMarkov.seed(stream);
+
+//{"message_id":10,"from":{"id":138266312,"is_bot":false,"first_name":"Yu Peng","username":"reqons","language_code":"en-SG"},"chat":{"id":138266312,"first_name":"Yu Peng","username":"reqons","type":"private"},"date":1539356512,"text":"f"}
+
+
+
+bot.on("text", (message) => {
+	var returnmsg = dresdenMarkov.respond(message.text).join(' ');
+	bot.sendMessage(message.chat.id, returnmsg);
+});
+
